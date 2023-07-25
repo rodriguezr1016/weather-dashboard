39b98a478e1938bba0ad939c4326d9eb4fd358cf
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-07-25 15:00:32 -0700

    I added some event listeners that create buttons with event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
 //I want to add the previous searches to a list
-var inputEl= document.getElementById('#search-input').value;
-var searchbtnEl= document.querySelector("#search-btn")
-searchbtnEl.addEventListener('click', function(event){
-    event.preventDefault();
-    console.log('hey');
+var searchbtnEl= document.querySelector("#searchbtn");
+searchbtnEl.addEventListener("click", function(target){
     createlist();
 })
 function createlist(){
-    localStorage.setItem('city-name',inputEl);
+    var inputValue= document.getElementById('search-input').value;
+    var inputEl = document.getElementById("search-input");
+    localStorage.setItem('city-name',inputValue);
+    var recentSearches= document.getElementById("recent-searches");
+    var liEl = document.createElement("li");
+    var newButton = document.createElement("button");
+    newButton.setAttribute("id", "search-again")
+    newButton.addEventListener("click", function(){
+        console.log("hello")
+    })
+    newButton.innerHTML = inputValue;
+    liEl.appendChild(newButton);
+    recentSearches.appendChild(liEl);
+    inputEl.value=""
 
 }
